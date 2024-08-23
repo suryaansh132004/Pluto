e68a8bc40c77a77ce5ae37c9ac2439be5285c86e
suryaansh132004 <83755524+suryaansh132004@users.noreply.github.com>
2024-08-24 02:18:18 +0530

    Update user.py

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=keys.api_key_gemini)
 model = genai.GenerativeModel('gemini-1.5-flash')
-system_role = "You are a female virtual assistant named Sidney skilled in general tasks like Alexa and Google Cloud."
+system_role = "You are a male virtual assistant named Pluto skilled in general tasks like Alexa and Google Cloud."
 user_role = "What is engineering?"
 response = model.generate_content(f"{system_role}. {user_role} ")
 print(response.text)
