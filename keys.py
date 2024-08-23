f4d8028b264235927ec33af05db6426afc641277
suryaansh132004 <83755524+suryaansh132004@users.noreply.github.com>
2024-08-24 02:17:25 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,7 @@
-api_key_gemini="AIzaSyBO49gwl_QhKXfzgbdVRmOtGbKVUN5LLHU"
-newsapi="a68ecb6bf52049f48104387a172a2f3c"
-weatherapi="c05661f78fc469cfedd96005f3a6d8e6"
-client_id="03b0f321902c4cfca84b0985c9cc4057"
-client_secret="ec289529016c463fbfd373265b06221a"
+#add your API keys below
+api_key_gemini=""
+newsapi=""
+weatherapi=""
+client_id=""
+client_secret=""
 
